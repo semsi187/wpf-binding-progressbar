f72449a25471da23e7d33817720cebe08103267a
stepguest <stepguest@STHQ012E-03>
2023-03-28 11:27:28 +0400

    Add project files.

---
+++ wpf binding progres bar/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpf_binding_progres_bar
+{
+    public partial class MainWindow : Window
+    {
+        private int _progress = 10;
+        public int Progress
+        {
+            get { return _progress; }
+            set
+            {
+                _progress = value;
+                OnPropertyChanged(nameof(Progress));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataContext = this;
+        }
+        private void ProgressBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            
+        }
+
+        private void downButton_Click(object sender, RoutedEventArgs e)
+        {
+            progbar.Value -= 1;
+        }
+
+        private void upButton_Click(object sender, RoutedEventArgs e)
+        {
+            progbar.Value += 1;
+        }
+
+    }
+}
